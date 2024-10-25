cae1b1af47239e04960df474c3a813be769119fb
beshoy_younan <beshoy.younan@outlook.com>
2024-10-25 09:50:20 -0300

    Initial commit

---
+++ src/main/java/com/besho/java/jpa_hibernate_advanced_mapping/JpaHibernateAdvancedMappingApplication.java
@@ -0,0 +1,13 @@
+package com.besho.java.jpa_hibernate_advanced_mapping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaHibernateAdvancedMappingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaHibernateAdvancedMappingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/besho/java/jpa_hibernate_advanced_mapping/JpaHibernateAdvancedMappingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.besho.java.jpa_hibernate_advanced_mapping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaHibernateAdvancedMappingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
